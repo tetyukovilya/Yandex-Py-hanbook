640ebab55f9dc1d4552c8bc7f5e8c3e019a652d2
tetyukovilya <tetyukov06@gmail.com>
2026-01-10 13:52:09 +0500

    T

---
+++ ❌T.py
@@ -0,0 +1,12 @@
+a = str(input())
+b = str(input())
+c = str(input())
+
+if (a > b  and a > b and 'зайка' in a or 'зайка') or a > c :
+    print(a, len(a))
+elif b > a and b > c and 'зайка' in b:
+    print(b, len(b))
+elif c > a and c > b and 'зайка' in c:
+    print(c, len(c))
+elif 'зайка' in (a, b):
+    print('gaz')
